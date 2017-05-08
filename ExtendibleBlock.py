8e47117a16b4b44ab72741411bade82ec4338b67
nemani07 <lnema216@live.kutztown.edu>
2017-05-08 19:25:39 -0400

    static final update
    static working
    Linear working

---
+++ ExtendibleBlock.py
@@ -0,0 +1,107 @@
+from Record import *
+
+class ExtendibleBlock:
+
+	def __init__(self, size, recordSize, fieldSize, bfr, depthSize, data):
+		self.size = size
+		# size of entire record (including hashing field)
+		self.recordSize = recordSize
+		self.fieldSize = fieldSize
+		self.bfr = bfr
+		self.data = data
+		self.depthSize = depthSize
+
+	def hasSpace(self):
+		for recNum in range(0, self.bfr):
+			aRecord = self.makeRecord(self.data[recNum*self.recordSize:(recNum+1)*self.recordSize])
+			if aRecord.isEmpty():
+				#print("New Rec num:" + str(recNum))
+				return recNum
+		return -1
+
+	# only to be used inside this class
+	# makes creating records easier and more clear
+	def makeRecord(self, data):
+		return Record(self.recordSize, self.fieldSize, False, data)
+
+	# return pointer value
+	def getLocalDepth(self):
+		return int.from_bytes(self.data[(-1*self.depthSize):], byteorder='big')
+	#returns boolean value
+	def isEmpty(self):
+		for recNum in range(0, self.bfr):
+			aRecord = self.makeRecord(self.data[recNum*self.recordSize:(recNum+1)*self.recordSize])
+			if not aRecord.isEmpty():
+				return False
+		return True
+
+	# returns an array of record objects
+	def getAllRecords(self):
+		records = []
+		for recNum in range(0, self.bfr):
+			aRecord = self.makeRecord(self.data[recNum*self.recordSize:(recNum+1)*self.recordSize])
+			if not aRecord.isEmpty():
+				records.append(aRecord)
+		return records
+		
+	# returns a dictionary of locations and record objects
+	def getAllRecordsWithLoc(self):
+		records = {}
+		i = 0
+		for recNum in range(0, self.bfr):
+			aRecord = self.makeRecord(self.data[recNum*self.recordSize:(recNum+1)*self.recordSize])
+			if not aRecord.isEmpty():
+				records[i] = aRecord
+			i += 1
+		return records	
+		
+	def getAllRecordsInclDeleted(self):
+		records = []
+		for recNum in range(0, self.bfr):
+			aRecord = self.makeRecord(self.data[recNum*self.recordSize:(recNum+1)*self.recordSize])
+			if aRecord.getHashValueEvenIfDeleted():
+				records.append(aRecord)
+		return records
+
+	def getRecordWithValue(self, value):
+		records = self.getAllRecords()
+		for record in records:
+			if record.getHashValue() == value:
+				return record
+				
+	def getRecordWithValueInclDeleted(self, value):
+		records = self.getAllRecordsInclDeleted()
+		for record in records:
+			if record.getHashValueEvenIfDeleted() == value:
+				return record
+	
+	def getRecordWithValueLoc(self, value):
+		records = self.getAllRecords()
+		i = 0
+		for record in records:
+			if record.getHashValue() == value:
+				return i
+			else:
+				i += 1
+
+	def getRecordWithValueLocInclDeleted(self, value):
+		records = self.getAllRecordsInclDeleted()
+		i = 0
+		for record in records:
+			if record.getHashValueEvenIfDeleted() == value:
+				return i
+			else:
+				i += 1
+	def containsRecordWithValue(self, value):
+		records = self.getAllRecords()
+		for record in records:
+			if record.getHashValue() == value:
+				return True
+		return False
+		
+	def containsRecordWithValueInclDeleted(self, value):
+		records = self.getAllRecordsInclDeleted()
+		for record in records:
+			if record.getHashValueEvenIfDeleted() == value:
+				return True
+		return False
--- hashing.py
+++ hashing.py
@@ -1,19 +1,43 @@
+from LinearlyHashedFile import *
 from StaticlyHashedFile import *
-# file=StaticlyHashedFile(256,100,10,10,'C:/LN/test1')
-
+from ExtendibleHashedFile import *
+import random
 
 def initStatic():
-	return
+	print("Please enter all sizes in bytes.")
+	blockSize = int(input("Enter the block size:  "))
+	recordSize = int(input("Enter the record size:  "))
+	while recordSize > blockSize:	
+		print("Record size must be smaller than block size")
+		recordSize = int(input("Enter the record size:  "))
+	fieldSize = int(input("Enter the size of the field that will be used for hashing: "))
+	while fieldSize > recordSize:	
+		print("Field size must be smaller than record size")
+		fieldSize = int(input("Enter the field size:  "))
+	fileSize = int(input("Enter the size of the file in number of blocks:  "))
+	path = input("Enter the path and name of the file:  ")
+	return StaticlyHashedFile(blockSize, recordSize, fieldSize, fileSize, path)
 
-def staticMenu(file):
-	return
-			
-			
-def initStatic():
+def initExtendible():
+	print("Please enter all sizes in bytes.")
+	blockSize = int(input("Enter the block size:  "))
+	recordSize = int(input("Enter the record size:  "))
+	while recordSize > blockSize:	
+		print("Record size must be smaller than block size")
+		recordSize = int(input("Enter the record size:  "))
+	fieldSize = int(input("Enter the size of the field that will be used for hashing: "))
+	while fieldSize > recordSize:	
+		print("Field size must be smaller than record size")
+		fieldSize = int(input("Enter the field size:  "))
+	path = input("Enter the path and name of the file:  ")
+	return ExtendibleHashedFile(blockSize, recordSize, fieldSize, path)
+
+def initLinear():
 	while True:
 		choice = input("Would you like to create a (N)ew file or use and (E)xisting one?  ")
 		if choice == 'E' or choice == 'e':
-			file=StaticlyHashedFile(256,100,10,10,'C:/LN/test1')
+			path = input("Enter the file location:  ")
+			return LinearlyHashedFile.fromExistingFile(path)
 		elif choice == 'N' or choice == 'n':
 			print("Please enter all sizes in bytes.")
 			blockSize = int(input("Enter the block size:  "))
@@ -26,41 +50,58 @@ def initStatic():
 				print("Field size must be smaller than record size")
 				fieldSize = int(input("Enter the field size:  "))
 			path = input("Enter the path and name of the file:  ")
-			return StaticlyHashedFile(blockSize, recordSize, fieldSize, None)
+			while True:
+				choice = input("Will your hashing field be of type string? (Y/N) ")
+				if choice == 'Y' or choice == 'y':
+					strKeys = True
+					break
+				elif choice == 'N' or choice == 'n':
+					strKeys = False
+					break
+				else:
+					print("Please make a valid selection (Y or N)")
+			return LinearlyHashedFile(blockSize, recordSize, fieldSize, path, strKeys, None)
 		else:
 			print("Please make a valid selection (N or E)")
-		
-def staticMenu(file):
+
+def menu(file, type):
 	while True:
-		print("Static Hashing Menu:")
+		print(str(type) + " Hashing Menu: ")
 		print("   1: Insert")
 		print("   2: Search")
 		print("   3: Update")
 		print("   4: Delete")
 		print("   5: Undelete")
 		print("   6: Display")
-		print("   7:Time Statastics")
+		print("   7: Print statistics")
 		print("   8: Quit")
+		choice = input("")
 		if choice == '1':
 			keyVal = input("Enter the key value for the record to be inserted:  ")
 			data = input("Enter the data to be stored with that value: ")
-			keyVal = int(keyVal)
+			if not hasattr(file, "strKeys") or not file.strKeys:
+				keyVal = int(keyVal)
 			file.insert(keyVal, data)
 		elif choice == '2':
 			keyVal = input("Enter the key value for the record to be search:  ")
-			keyVal = int(keyVal)
+			if not hasattr(file, "strKeys") or not file.strKeys:
+				keyVal = int(keyVal)
 			file.search(keyVal)
 		elif choice == '3':
 			keyVal = input("Enter the key value for the record to be updated:  ")
 			data = input("Enter the data to be stored with that value: ")
-			keyVal = int(keyVal)
+			if not hasattr(file, "strKeys") or not file.strKeys:
+				keyVal = int(keyVal)
 			file.update(keyVal, data)
 		elif choice == '4':
 			keyVal = input("Enter the key value for the record to be deleted:  ")
+			if not hasattr(file, "strKeys") or not file.strKeys:
+				keyVal = int(keyVal)
 			file.delete(keyVal)
 		elif choice == '5':
 			keyVal = input("Enter the key value for the record to be undeleted:  ")
-			keyVal = int(keyVal)
+			if not hasattr(file, "strKeys") or not file.strKeys:
+				keyVal = int(keyVal)
 			file.undelete(keyVal)
 		elif choice == '6':
 			while True:
@@ -99,75 +140,43 @@ def staticMenu(file):
 					break
 				else:
 					print("Please make a valid selection (Y or N)")
-			file.setStatistics(times, workings)	
+			file.setStatistics(times, workings)
 		elif choice == '8':
 			break
 		else:
 			print("Please make a valid selection (1-8)")
+
+def chooseScheme():
+	while True:
+		print("Choose a hashing scheme:")
+		print("   (S)tatic Hashing")
+		print("   (E)xtendible Hashing")
+		print("   (L)inear Hashing")
+		choice = input("")
+		if choice == 'S' or choice == 's':
+			return {"file": initStatic(), "type": "Static"}
+		elif choice == 'E' or choice == 'e':
+			return {"file": initExtendible(), "type": "Extendible"}
+		elif choice == 'L' or choice == 'l':
+			return {"file": initLinear(), "type": "Linear"}
+		else:
+			print("Please make a valid selection (S, E, or L)")
+
+while True:
+	fileAndType = chooseScheme()
+	file = fileAndType["file"]
+	menu(file, fileAndType["type"])
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
-		
-# file.numOfBlocksare()
-# file.insert(60, "test val holy shit holy shit holy shittttt")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-# for i in range(1,30):
-	# file.insert(int(i), "test val")		
-# file.search(10)
-# file.delete(20)
-# file.search(20)
-# file.undelete(20)
-# file.displayHeader()
-# file.displayBlock(i)
-
-# file.display(False)
-
-# newFile = StaticlyHashedFile.fromExistingFile('C:/LN/test1')
-# newFile.display(True)
-
-
-
-
-
-
-
-
-
-# lst = [12,34,200,255,203]
-
-# strfile = 'C://RF/test.txt'
-# buffer = bytes(lst)
-
-# print(buffer)
-
-# with open(strfile,'bw') as f:
-    # f.write(buffer)
-
-# print('File written, reading it back')
-
-# with open(strfile,'br') as f:
-    # buffer = f.read(16)
-    # print("Length of buffer is %d" % len(buffer))
-
-    # for i in buffer:
-        # print(int(i))
