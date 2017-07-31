21bb4e93fb0ee9041792623065de882aa23855cc
RoyFossil <bamfpita@gmail.com>
2017-07-31 15:14:08 -0400

    fix for possible bfr calculation error
    needs to be based off the blockSize that doesn't include the local depth.

--- ExtendibleHashedFile.py
+++ ExtendibleHashedFile.py
@@ -13,7 +13,7 @@ class ExtendibleHashedFile:
 		# record size supplied by user should include the hash field size and deletion marker
 		self.recordSize = recordSize
 		self.fieldSize = fieldSize
-		self.bfr = math.floor(self.blockSize/self.recordSize)
+		self.bfr = math.floor(blockSize/self.recordSize)
 		self.globalDepth = 0
 		self.nextAvailableBucket = 3
 		self.times = False
